79e33dc28ca6e7a8327afcc191a44f481bf7f47c
Andy Lau <andyolau@gmail.com>
2024-01-30 14:49:56 -0500

    added psycopg2-binary to talk to postgres, added a new route to add user to the table with no duplicates ability

--- app.py
+++ app.py
@@ -1,14 +1,58 @@
-from flask import Flask
+import os 
+import psycopg2
+from dotenv import load_dotenv
+from flask import Flask, request
+
+CREATE_USERS_TABLE = (
+    "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT);"
+)
+
+# value for name is currently a placeholder, have to provide value during execution
+INSERT_USER_RETURN_ID = (
+    "INSERT INTO users (name) VALUES (%s) RETURNING id;"
+)
+
+# searches the user table and return an id for the row (if exists) where the name is matched
+CHECK_IF_USER_EXISTS = (
+    "SELECT id FROM users WHERE name = %s;"
+)
+
+# read in environment variables in .env 
+load_dotenv()
 
 app = Flask(__name__)
+url = os.getenv("DATABASE_URL")
+# connect to the postgres db 
+connection = psycopg2.connect(url)
+
 
 @app.get("/")
 def home(): 
     return "Home Page"
 
-@app.get("/create-user")
+@app.post("/api/create-user")
 def create_user(): 
-    return "Please enter a username"
-
+    # for now data input is in json via postman, get_json makes the data a dictionary to easier parsing
+    data = request.get_json() 
+    user_name = data["name"]
+    with connection: 
+        # a cursor allows us to insert or retrieve data from the db
+        with connection.cursor() as cursor:
+            # creates the room only once because we specified it in the query
+            cursor.execute(CREATE_USERS_TABLE)
+            
+            # checks if the username already exists
+            cursor.execute(CHECK_IF_USER_EXISTS, (user_name,))
+            # fetchone() fetches the result of the previous function, only returns id or None
+            existing_user_id = cursor.fetchone()
+            if existing_user_id: 
+                return {"message": f"User: '{user_name}' already exists"}, 400
+            
+            # if username doesn't exist, add the new user
+            cursor.execute(INSERT_USER_RETURN_ID, (user_name,))
+            # retrieve the id, fetchone() returns only one row, but this works because we only added one row
+            # [0] to avoid tuple 
+            user_id = cursor.fetchone()[0]
+    return {"id": user_id, "message": f"User: {user_name} created"}, 201
 
 
