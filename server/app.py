9f824df782e9c0350fc11c0c929081f0f476034a
Michael Singer <98486724+mis825@users.noreply.github.com>
2024-01-31 11:04:39 -0500

    New main (#1)
    * added gitignore and test file for testing venv
    
    * created simple flask app with super basic routes, deleted test.py, added requirements.txt for dependency management and ease of collaboration
    
    * configure flask app to use debug mode
    
    * added psycopg2-binary to talk to postgres, added a new route to add user to the table with no duplicates ability
    
    * updated readme to include instructions and configurations
    
    * updated requirements.txt to include the most recent dependencies
    
    * reconfigured .flaskenv
    
    * updated readme to have usage instructions
    
    * fixed syntax bug in return message for deleting user successfully
    
    * delete-user changed app.post to app.delete
    
    * make new main directory structure
    
    * added index.html and sockets to send messages
    
    * moved templates folder into server folder in order for the render template method to work, edit f string for log comments
    
    * moved .gitignore to root
    
    * added web folder
    
    ---------
    
    Co-authored-by: Andy Lau <andyolau@gmail.com>

---
+++ server/app.py
@@ -0,0 +1,93 @@
+import os 
+import psycopg2
+from dotenv import load_dotenv
+from flask import Flask, request, render_template
+from flask_socketio import SocketIO, send
+
+CREATE_USERS_TABLE = (
+    "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT);"
+)
+
+# value for name is currently a placeholder, have to provide value during execution
+INSERT_USER_RETURN_ID = (
+    "INSERT INTO users (name) VALUES (%s) RETURNING id;"
+)
+
+# searches the user table and return an id for the row (if exists) where the name is matched
+CHECK_IF_USER_EXISTS = (
+    "SELECT id FROM users WHERE name = %s;"
+)
+
+# delete the user if it exists via its row id
+DELETE_USER = (
+    "DELETE FROM users WHERE id = %s;"
+)
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET'] = "secret123"
+socketio = SocketIO(app, cors_allowed_origins="*")
+url = os.getenv("DATABASE_URL")
+connection = psycopg2.connect(url)
+
+@socketio.on('message')
+def handle_message(message):
+    print("Received message: " + message)
+    if message != "User connected!":
+        send(message, broadcast=True)
+        
+
+@app.route("/")
+def index(): 
+    return render_template("index.html") 
+
+@app.post("/api/create-user")
+def create_user(): 
+    # for now data input is in json via postman, get_json makes the data a dictionary to easier parsing
+    data = request.get_json() 
+    user_name = data["name"]
+    
+    with connection: 
+        # a cursor allows us to insert or retrieve data from the db
+        with connection.cursor() as cursor:
+            # creates the room only once because we specified it in the query
+            cursor.execute(CREATE_USERS_TABLE)
+            
+            # checks if the username already exists
+            cursor.execute(CHECK_IF_USER_EXISTS, (user_name,))
+            # fetchone() fetches the result of the previous function, only returns id or None
+            existing_user_id = cursor.fetchone()
+            if existing_user_id: 
+                return {"message": f"User: '{user_name}' already exists"}, 400
+            
+            # if username doesn't exist, add the new user
+            cursor.execute(INSERT_USER_RETURN_ID, (user_name,))
+            # retrieve the id, fetchone() returns only one row, but this works because we only added one row
+            # [0] to avoid tuple 
+            user_id = cursor.fetchone()[0]
+            
+    return {"id": user_id, "message": f"User: '{user_name}' created"}, 201
+
+@app.delete("/api/delete-user")
+def delete_user():
+    data = request.get_json()
+    user_name = data["name"]
+    
+    with connection: 
+        with connection.cursor() as cursor: 
+            # check if the user exists
+            cursor.execute(CHECK_IF_USER_EXISTS, (user_name,))
+            existing_user_id = cursor.fetchone()
+            
+            if not existing_user_id:
+                return {"message": f"User: '{user_name}' not found"}, 404
+            
+            user_id = existing_user_id[0]
+            cursor.execute(DELETE_USER, (user_id,))
+    
+    return {"message": f"User: '{user_name}' deleted successfully"}, 200
+
+if __name__ == "__main__":
+    socketio.run(app, host="128.180.235.203")
+            
