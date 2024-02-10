95db7ce3de4fb0732e819c10ffb007c05f942613
andy-1au <andyolau888@gmail.com>
2024-02-10 14:21:26 -0500

    made arguments more robust

--- server/cli.py
+++ server/cli.py
@@ -3,21 +3,21 @@ from db_utils import create_user, delete_user, get_user, list_tables, print_tabl
 
 def main(): 
     parser = argparse.ArgumentParser(description="CLI for user management")
-    parser.add_argument('action', choices=['list', 'print', 'create', 'delete', 'get'], help='Action to perform')
+    parser.add_argument('action', choices=['list_tables', 'print_table', 'create_user', 'delete_user', 'get_user'], help='Action to perform')
     args = parser.parse_args()
     
-    if args.action == 'list':
+    if args.action == 'list_tables':
         list_tables()
-    elif args.action == 'print':
+    elif args.action == 'print_table':
         table_name = input("Enter the table name: ")
         print_table_contents(table_name) 
-    elif args.action == 'create':
+    elif args.action == 'create_user':
         name = input("Enter user name to add: ")
         create_user(name)
-    elif args.action == 'delete':
+    elif args.action == 'delete_user':
         name = input("Enter user name to delete: ")
         delete_user(name)
-    elif args.action == 'get':
+    elif args.action == 'get_user':
         name = input("Enter user name to get: ")
         get_user(name)
 
